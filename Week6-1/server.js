6591034ed6bddb37401690dd1d79db279f589397
Jo. Sabana <seffy.sabana@gmail.com>
2025-04-14 03:47:59 +1000

    Week 6 Activity

---
+++ Week6-1/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+require('dotenv').config(); // At the top of your app
+
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+
+
+
+const dbURI = process.env.MONGO_URI;
+
+mongoose.connect(dbURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set view engine (EJS for dynamic templates)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', authRoutes);
+
+// Healthcheck endpoint for Docker
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
